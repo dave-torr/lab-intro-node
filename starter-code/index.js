56accaf5287ca96098e1195f45ca2beb2b88a4a6
David Torres <djjabbua@gmail.com>
2020-03-02 17:52:50 -0500

    lab node

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items= []
+    this.length=0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(( a , b ) => a-b )
+    this.length = this.items.length
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if(!this.items[pos]){
+      throw new Error('OutOfBounds')
+    }
+    return this.items[pos]
+  }
 
-  min() {}
+  max() {
+    if(this.items.length ===0){
+      throw new Error('EmptySortedList')
+    }
+    return this.items[this.items.length-1]
+  }
 
-  sum() {}
 
-  avg() {}
+  min() {
+    if(this.items.length ===0){
+      throw new Error('EmptySortedList')
+    }
+      return this.items[0]
+  }
+
+  sum() {
+    let sum = this.items.reduce(function(acc, curr){
+      return acc + curr
+    }, 0)
+    return sum
+  }
+
+  avg() {
+    if(this.items.length ===0){
+      throw new Error('EmptySortedList')
+    }
+    let sum = this.items.reduce(function(acc, curr){
+      return acc + curr
+    }, 0)
+    return sum/this.items.length
+  }
 }
 
 module.exports = SortedList;
